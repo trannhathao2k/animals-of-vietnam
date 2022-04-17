fa0cfa542e5b97fbbb94e19118b6cbd4bf49b81f
Nhat Hao <nhathao2709@gmail.com>
2022-04-17 21:20:55 +0700

    Upload by NhatHao - 17/04

--- chitiet.php
+++ chitiet.php
@@ -1,5 +1,5 @@
 <?php
-$sql_chitiet = "SELECT * FROM dongvat, hinhanh WHERE dongvat.ma_dv = hinhanh.ma_dv AND dongvat.ma_dv='$_GET[id]' LIMIT 1";
+$sql_chitiet = "SELECT * FROM dongvat WHERE dongvat.ma_dv='".$_GET["id"]."' LIMIT 1";
 $query_chitiet = mysqli_query($mysqli, $sql_chitiet);
 while ($row_chitiet = mysqli_fetch_array($query_chitiet)) {
 ?>
@@ -13,16 +13,19 @@ while ($row_chitiet = mysqli_fetch_array($query_chitiet)) {
                         $anh = $_GET['anh'];
                     }
                     else {
-                        $anh = $row_chitiet['ten_image'];
+                        $sql_anh_dv = "SELECT * FROM hinhanh WHERE ma_dv = '.$_GET[id].' LIMIT 1";
+                        $query_anh_dv = mysqli_query($mysqli, $sql_anh_dv);
+                        $row_anh_dv = mysqli_fetch_array($query_anh_dv);
+                        $anh = $row_anh_dv['ten_image'];
                     }
                 ?>
                 <img src="./img/animals/<?php echo $anh ?>"
-                    alt="anhdongvat">
+                    alt="anhdongvat" id="image" style="width: 400px; height: 400px; display: block; margin: 20px 0 20px 10px; border-radius: 10px">
             </div>
             <div class="d-flex flex-wrap justify-content-around anhnho"
                 style="background-color: #CDEDED; margin:10px 10px 0 10px; padding:10px;">
                 <?php
-                    $sql_anh = "SELECT ten_image FROM hinhanh WHERE ma_dv='$_GET[id]'";
+                    $sql_anh = "SELECT ten_image FROM hinhanh WHERE ma_dv='".$_GET["id"]."'";
                     $query_anh = mysqli_query($mysqli, $sql_anh);
                     if (mysqli_num_rows($query_anh)==0) {
                         ?>
@@ -33,8 +36,10 @@ while ($row_chitiet = mysqli_fetch_array($query_chitiet)) {
                         
                         while($row_anh = mysqli_fetch_array($query_anh)){
                             ?>
+                            <!-- <button onclick="myFunction(<?php //echo $row_anh['ten_image'] ?>)">
+                            </button> -->
                                 <a href="?route=chitiet&id=<?php echo $row_chitiet['ma_dv']?>&anh=<?php echo $row_anh['ten_image'] ?>">
-                                    <img src="./img/animals/<?php echo $row_anh['ten_image'] ?>" alt="anhdongvat">
+                                    <img src="./img/animals/<?php echo $row_anh['ten_image'] ?>" alt="anhdongvat" style="border: 2px solod white">
                                 </a>
                                 
                             <?php
@@ -46,7 +51,7 @@ while ($row_chitiet = mysqli_fetch_array($query_chitiet)) {
                 Ảnh được thêm bởi <a href="">&#169;
                     <?php
                         $sql_ctv = "SELECT * FROM themdongvat, obvervation
-                        WHERE themdongvat.ma_ctv = obvervation.ma_ctv AND ma_dv = '$_GET[id]'";
+                        WHERE themdongvat.ma_ctv = obvervation.ma_ctv AND ma_dv = '".$_GET["id"]."'";
                         $query_ctv = mysqli_query($mysqli, $sql_ctv);
                         $row_ctv = mysqli_fetch_array($query_ctv);
                         echo $row_ctv['hoten_ctv'];
@@ -193,7 +198,12 @@ while ($row_chitiet = mysqli_fetch_array($query_chitiet)) {
                 <b>Phân bố</b>
             </h2>
             <div>
-                <img src="./img/bando.png" alt="" class="bando">
+                <iframe src="<?php
+                    $sql_toado = "SELECT * FROM toado WHERE ma_dv = '$_GET[id]'";
+                    $query_toado = mysqli_query($mysqli, $sql_toado);
+                    $row_toado = mysqli_fetch_array($query_toado);
+                    echo $row_toado['ten_toado'];          
+                ?>" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
     </div>
